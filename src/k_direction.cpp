1684693d44232aa613a07badc9f605b203fd70ae
Kyle Gullion <kgullion@gmail.com>
2024-04-22 17:13:32 -0400

    add klein

--- src/k_direction.cpp
+++ src/k_direction.cpp
@@ -4,12 +4,21 @@
 using namespace godot;
 
 void Kdirection::_bind_methods() {
+	// ClassDB::bind_method(D_METHOD("x"), &Kdirection::x);
+	// ClassDB::bind_method(D_METHOD("y"), &Kdirection::y);
+	// ClassDB::bind_method(D_METHOD("z"), &Kdirection::z);
+	// ClassDB::bind_method(D_METHOD("normalize"), &Kdirection::normalize);
+	// ClassDB::bind_method(D_METHOD("normalized"), &Kdirection::normalized);
 }
 
-Kdirection::Kdirection() {
-	// Initialize any variables here.
-}
+Kdirection::Kdirection() noexcept { dir = kln::direction(); }
+Kdirection::Kdirection(float x, float y, float z) noexcept { dir = kln::direction(); }
+// Kdirection::Kdirection(kln::direction d) noexcept { dir = d; }
+Kdirection::~Kdirection() { }
 
-Kdirection::~Kdirection() {
-	// Add your cleanup here.
-}
+// float Kdirection::x() const noexcept { return dir.x(); }
+// float Kdirection::y() const noexcept { return dir.y(); }
+// float Kdirection::z() const noexcept { return dir.z(); }
+
+// void Kdirection::normalize() noexcept { dir.normalize(); }
+// Kdirection Kdirection::normalized() const noexcept { return Kdirection(dir.normalized()); }
--- src/k_direction.h
+++ src/k_direction.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <godot_cpp/classes/ref_counted.hpp>
+#include <klein/klein.hpp>
 
 namespace godot {
 
@@ -10,10 +11,22 @@ class Kdirection : public RefCounted {
 protected:
 	static void _bind_methods();
 
-public:
-	Kdirection();
-	~Kdirection();
+private:
+	kln::direction dir = kln::direction();
 
+public:
+	Kdirection() noexcept;
+	Kdirection(float x, float y, float z) noexcept;
+	// Kdirection(kln::direction dir) noexcept;
+
+
+	~Kdirection();
+    // [[nodiscard]] float x() const noexcept;
+	// [[nodiscard]] float y() const noexcept;
+	// [[nodiscard]] float z() const noexcept;
+
+	// void normalize() noexcept;
+	// Kdirection normalized() const noexcept;
 };
 
 }
